0b5c9048fe543de3820ed7cda1a9c5ebd7d25489
Geoffrey T. Wark <geoff@geoffwark.com>
2017-06-14 00:45:50 -0400

    Add comments/source

--- nocaps_delayed.sh
+++ nocaps_delayed.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# This script changes Caps Lock into another Ctrl key after a 3 second delay.
+# NOTE: It should pick up your layout so no need to think about it.
+# SOURCE= https://www.emacswiki.org/emacs/MovingTheCtrlKey#toc6
+
 sleep 3
 setxkbmap -layout "$(setxkbmap -print | awk -F + '/xkb_symbols/ {print $2}')" -option ctrl:nocaps
